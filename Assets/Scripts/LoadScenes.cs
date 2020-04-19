9be2008b8029ac5076f62baaa9dfbbc656da1911
Arthur <grommelgod@gmail.com>
2020-04-19 18:36:27 +0200

    main menu

---
+++ Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScenes : MonoBehaviour
+{
+    [SerializeField]
+    private Image _tutorial;
+    [SerializeField]
+    private GameObject _button;
+ 
+    public void Tutorial()
+    {
+        _tutorial.enabled = true;
+        _button.SetActive(true);
+    }
+
+    public void StartScene()
+    {
+        Debug.Log("lol");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
