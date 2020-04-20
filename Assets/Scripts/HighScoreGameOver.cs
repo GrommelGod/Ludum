14e1d709c9ab3002bdb77056efdca29a6109a9d2
Arthur <grommelgod@gmail.com>
2020-04-20 20:21:57 +0200

    Last test

--- Assets/Scripts/HighScoreGameOver.cs
+++ Assets/Scripts/HighScoreGameOver.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class HighScoreGameOver : MonoBehaviour
 {
     private Text _text;
-    
+
     private void Start()
     {
         _text = GetComponent<Text>();
@@ -15,6 +15,13 @@ public class HighScoreGameOver : MonoBehaviour
 
     void Update()
     {
-        _text.text = "We can survive for : " + GameStats.Instance.points + "days ! That's not enough ! =O";
+        if (GameStats.Instance.points < 200)
+        {
+            _text.text = "We can survive for : " + GameStats.Instance.points + " days ! That's not enough ! =O";
+        }
+        else if (GameStats.Instance.points > 200)
+        {
+            _text.text = "Woaw ! You got enough for " + GameStats.Instance.points + " days ! But you forgot to buy Animal Passing... =/";
+        }
     }
 }
